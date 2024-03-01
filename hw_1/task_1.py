cdc583d3e5edd2a37a60c3b36119c1d79e546536
«Santimos» <stosoleg@gmail.com>
2024-03-01 15:36:24 +0300

    hw1

---
+++ hw_1/task_1.py
@@ -0,0 +1,29 @@
+"""
+Задание 1.
+Условие:
+Написать функцию на Python, которой передаются в качестве параметров команда и текст.
+Функция должна возвращать True, если команда успешно выполнена и текст найден в её выводе и False
+в противном случае. Передаваться должна только одна строка, разбиение вывода использовать не нужно.
+"""
+
+import subprocess
+
+
+def func(command, text):
+    res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
+    out = res.stdout
+    print(out)
+    if not res.returncode:
+        if text in out:
+            return True
+        else:
+            return False
+    else:
+        print("FAIL! Code isn't 0")
+
+
+if __name__ == "__main__":
+    command_1 = "ls -la"
+    text_1 = "dr"
+
+    print(func(command_1, text_1))
